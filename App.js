611e135969f4bfe5b8ca59172e87c17e50ffe188
real-tea <akash.0711a@gmail.com>
2023-03-14 13:15:14 +0530

    v1.0 completed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { KeyboardAvoidingView , StyleSheet, Text, View , Keyboard ,  TouchableOpacity , ScrollView , TextInput } from 'react-native';
 import Task from './components/Task';
 
@@ -16,7 +16,7 @@ export default function App() {
     setTask(null);
   }
 
-  const completeTask = () => {
+  const completeTask = (index) => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index , 1)
     setTaskItems(itemsCopy);
@@ -61,7 +61,10 @@ export default function App() {
         <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText = {text => setTask(text)}/>
 
           <TouchableOpacity onPress={()=>handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
 
+            </View>
           </TouchableOpacity>
 
       </KeyboardAvoidingView>
@@ -85,5 +88,34 @@ const styles = StyleSheet.create({
   },
   items : {
     marginTop : 30,
-  }
+  },
+  writeTaskWrapper : {
+    position : 'absolute',
+    bottom : 60,
+    width : '100%',
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+    alignItems : 'center'
+
+  },
+  addWrapper : {
+    paddingVertical : 15 , 
+    paddingHorizontal : 15,
+    backgroundColor : '#fff',
+    borderRadius : 60,
+    justifyContent : 'center',
+    alignItems : 'center',
+    borderColor : '#C0C0C0',
+    borderWidth : 1,
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addText : {}
 });
